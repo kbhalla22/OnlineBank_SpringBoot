e376edd2f0bc2ea137027640795f51592e6d9290
karanbhalla <kbhalla22@gmail.com>
2019-06-18 13:11:18 -0400

    Updated the controller class for handling signup page

--- userFront/src/main/java/com/controller/HomeController.java
+++ userFront/src/main/java/com/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userFront.domain.User;
 
 @Controller
 public class HomeController {
@@ -14,5 +18,11 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
-	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		User user=new User();
+		model.addAttribute("user",user);
+		return "signup";
+		
+	}
 }
