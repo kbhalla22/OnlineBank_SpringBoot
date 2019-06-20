ccfcc3e70c65f7bb12e7a6e20c50e497d5cac969
karanbhalla <kbhalla22@gmail.com>
2019-06-19 20:00:14 -0400

    updated the controller class

---
+++ userFront/src/main/java/com/userFront/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.userFront.controller;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.userFront.domain.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "signup";
+    }
+
+
+
+
+}
