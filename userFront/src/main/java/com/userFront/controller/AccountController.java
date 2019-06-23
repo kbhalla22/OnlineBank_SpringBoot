9b03a74bbf4a43332983c7d48252f69e8a016bdf
karanbhalla <kbhalla22@gmail.com>
2019-06-23 17:30:29 -0400

    Updated the code for saving savings transactions to database

--- userFront/src/main/java/com/userFront/controller/AccountController.java
+++ userFront/src/main/java/com/userFront/controller/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userFront.domain.PrimaryAccount;
+import com.userFront.domain.PrimaryTransaction;
 import com.userFront.domain.SavingsAccount;
 import com.userFront.domain.SavingsTransaction;
 import com.userFront.domain.User;
@@ -32,20 +33,24 @@ public class AccountController {
 
 	@RequestMapping("/primaryAccount")
 	public String primaryAccount(Model model,Principal principal) {
-		List<SavingsTransaction> savingsTransactionList = transactionService.findSavingsTransactionList(principal.getName());
-		User user=userService.findByUsername(principal.getName());
+		List<PrimaryTransaction> primaryTransactionList = transactionService.findPrimaryTransactionList(principal.getName());
+		User user = userService.findByUsername(principal.getName());
 		PrimaryAccount primaryAccount=user.getPrimaryAccount();
 		model.addAttribute("primaryAccount", primaryAccount);
+		model.addAttribute("primaryTransactionList", primaryTransactionList);
 		return"primaryAccount";
 	}
 	@RequestMapping("/savingsAccount")
-	public String savingsAccount(Model model,Principal principal) {
-		User user=userService.findByUsername(principal.getName());
-		SavingsAccount savingsAccount= user.getSavingsAccount();
-		model.addAttribute("savingsAccount", savingsAccount);
-		return"savingsAccount";
-	}
+    public String savingsAccount(Model model, Principal principal) {
+		List<SavingsTransaction> savingsTransactionList = transactionService.findSavingsTransactionList(principal.getName());
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
 
+        model.addAttribute("savingsAccount", savingsAccount);
+        model.addAttribute("savingsTransactionList", savingsTransactionList);
+
+        return "savingsAccount";
+    }
 	@RequestMapping(value = "/deposit", method = RequestMethod.GET)
     public String deposit(Model model) {
         model.addAttribute("accountType", "");
