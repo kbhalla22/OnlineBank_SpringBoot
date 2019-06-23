e5fc962f9cc53db68eb25583315c92e770aa6903
karanbhalla <kbhalla22@gmail.com>
2019-06-23 12:23:15 -0400

    Updated the controller class to add mapping for userFront.html

--- userFront/src/main/java/com/userFront/controller/HomeController.java
+++ userFront/src/main/java/com/userFront/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.userFront.controller;
 
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userFront.dao.RoleDao;
+import com.userFront.domain.PrimaryAccount;
+import com.userFront.domain.SavingsAccount;
 import com.userFront.domain.User;
 import com.userFront.domain.security.UserRole;
 import com.userFront.service.UserService;
@@ -66,6 +69,18 @@ public class HomeController {
 
             return "redirect:/";
         }
+    }
+	
+	@RequestMapping("/userFront")
+	public String userFront(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        model.addAttribute("savingsAccount", savingsAccount);
+
+        return "userFront";
     }
 
 
