f8c15353ba15dc3b6473fe1ccb9b542c558a823e
karanbhalla <kbhalla22@gmail.com>
2019-06-23 15:38:08 -0400

    updated the service class for deposit method

--- userFront/src/main/java/com/userFront/service/AccountService.java
+++ userFront/src/main/java/com/userFront/service/AccountService.java
@@ -1,10 +1,13 @@
 package com.userFront.service;
 
+import java.security.Principal;
+
 import com.userFront.domain.PrimaryAccount;
 import com.userFront.domain.SavingsAccount;
 
 public interface AccountService {
 	PrimaryAccount createPrimaryAccount();
 	SavingsAccount createSavingsAccount();
+	void deposit(String accountType,double amount,Principal pricipal);
 
 }
