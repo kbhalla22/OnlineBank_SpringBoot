63a11d1d856cadebbcbf3a8df431b789f2306bc6
karanbhalla <kbhalla22@gmail.com>
2019-06-23 12:51:33 -0400

    Updated controller class to handle primary Account data

--- userFront/src/main/java/com/userFront/controller/AccountController.java
+++ userFront/src/main/java/com/userFront/controller/AccountController.java
@@ -1,14 +1,28 @@
 package com.userFront.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userFront.domain.PrimaryAccount;
+import com.userFront.domain.User;
+import com.userFront.service.UserService;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("/primaryAccount")
-	public String primaryAccount() {
+	public String primaryAccount(Model model,Principal principal) {
+		User user=userService.findByUsername(principal.getName());
+		PrimaryAccount primaryAccount=user.getPrimaryAccount();
+		model.addAttribute("primaryAccount", primaryAccount);
 		return"primaryAccount";
 	}
 	@RequestMapping("/savingsAccount")
